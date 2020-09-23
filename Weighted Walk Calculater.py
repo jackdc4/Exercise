d15ed250eec26502ef2d88e29fac9127f0057e2e
jackdc4 <71437074+jackdc4@users.noreply.github.com>
2020-09-23 15:22:12 +0100

    Calorie calculator for walking with a backpack
    A calculator built using the US army formula for calculating the calories burned on various surfaces and carrying various weights, currently built to print the calories burned in an hour walking at a constant speed of below 2.5mph which is well below average walking speed (3-4mph)

---
+++ Weighted Walk Calculater.py
@@ -0,0 +1,34 @@
+H = 1
+#Hours spent walking
+W = 115
+#Weight in kilos
+L = 10
+#Weight of Load
+V = 1
+#Speed in m/s
+n = 1
+#Terrain factor, paved roads being 1, gravel roads being 1.2
+G = 1
+#Gradient of roads, averaged throughout walk
+M = (1.5*W) + (2.0*(W+L))*((L/W)**2) + (n*(W+L)) * ((1.5*V**2)+(0.35*V)*G)
+#M is the metabolic rate, aka, KCal per Hour
+A = M*H
+#Amount of calories burned on the walk
+Speak = ("With the current numbers you will burn " + str(A) + " KCal per Hour walking!")
+print (Speak)
+
+#K =
+##Kilos to be converted to lbs
+#KPH =
+##Your speed in Kilometers per hour
+#MPH =
+##Your speed in Miles per hour
+#WeighLbs-K = 2.2 * K
+#Kph-m/s = 0.277778 * KPH
+#Mph-m/s = 0.44704 * MPH
+
+#print (WeightLbs-K)
+#print (Kph-m/s)
+#print (Mph-m/s)
+
+
